e0c0c4eb996d2daf9fc8a1e20fd6ab353f6ee6ec
Faz <faz888@gmail.com>
2025-12-12 03:18:09 +0500

    test(mock_api): Implement all required Discord API callbacks
    Add implementations for all @callback functions required by the
    RaffleBot.Discord.Api behaviour:
    - create_message/3
    - edit_message/4
    - delete_message/3
    - create_interaction_response/3
    - edit_interaction_response/2
    - get_user/1
    
    All 7 tests passing.

--- apps/raffle_bot/test/support/mock_api.ex
+++ apps/raffle_bot/test/support/mock_api.ex
@@ -4,4 +4,34 @@ defmodule RaffleBot.Discord.MockApi do
   """
 
   @behaviour RaffleBot.Discord.Api
+
+  @impl true
+  def create_message(_channel_id, _content, _opts \\ []) do
+    {:ok, %{}}
+  end
+
+  @impl true
+  def edit_message(_channel_id, _message_id, _content, _opts \\ []) do
+    {:ok, %{}}
+  end
+
+  @impl true
+  def delete_message(_channel_id, _message_id, _opts \\ []) do
+    {:ok, %{}}
+  end
+
+  @impl true
+  def create_interaction_response(_interaction, _response_type, _data) do
+    :ok
+  end
+
+  @impl true
+  def edit_interaction_response(_interaction, _data) do
+    :ok
+  end
+
+  @impl true
+  def get_user(_user_id) do
+    {:ok, %{}}
+  end
 end
